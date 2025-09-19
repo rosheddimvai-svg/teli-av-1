9294cd18cd14101ce5ca88aba33c1fcb200bc6f1
rosheddimvai-svg <rosheddimvai@gmail.com>
2025-09-19 19:31:20 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ from telegram.ext import (
 )
 
 # আপনার বট টোকেন এবং চ্যানেল আইডি
-BOT_TOKEN = "8197222627:AAGjX1XrAqlNnpMYpjSKjA4yOisfeTJbQEk"
-MAIN_CHANNEL_ID = -1002323042564 # মূল প্রাইভেট চ্যানেল
+BOT_TOKEN = "8245233994:AAHkeQIHd1M3yjJmfiW6iu_vTq5s4o3HovY"
+MAIN_CHANNEL_ID = -1002460901479 # মূল প্রাইভেট চ্যানেল
 ADMIN_NOTIFY_CHANNEL_ID = -1002787846366 # অ্যাডমিন নোটিফিকেশনের জন্য দ্বিতীয় চ্যানেল
 
 # অ্যাডমিনের ইউজারনেম, ওয়েবসাইটের লিঙ্ক এবং ভিআইপি গ্রুপের লিঙ্ক
 ADMIN_USERNAME = "rs_rezaul_99"
-HACK_WEBSITE_URL = "https://as-official-channel.netlify.app/"
-VIP_GROUP_LINK = "https://t.me/+WBrUuuuIn2IwMjg1"
+HACK_WEBSITE_URL = "https://zesty-kelpie-7f5595.netlify.app/"
+VIP_GROUP_LINK = "vip group link"
 REFERRAL_LINK = "https://dkwin12.com/#/register?invitationCode=82626111964"
 
 # ConversationHandler এর জন্য স্টেট
@@ -201,3 +201,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
